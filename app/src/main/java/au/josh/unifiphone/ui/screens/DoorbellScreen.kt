fbc5a911f7c33253be65ce946cc849eed790df9f
Josh <43034295+josh2893@users.noreply.github.com>
2026-07-16 20:34:57 +0800

    Remove unused Compose layout imports

--- app/src/main/java/au/josh/unifiphone/ui/screens/DoorbellScreen.kt
+++ app/src/main/java/au/josh/unifiphone/ui/screens/DoorbellScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.foundation.shape.CircleShape
